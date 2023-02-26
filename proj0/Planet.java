26793ffad8b534b08d2243fa4fe2e75e34b7a2a9
Lzccc <109495689+LinZC24@users.noreply.github.com>
2023-02-26 10:17:02 +0800

    Add files via upload

---
+++ proj0/Planet.java
@@ -0,0 +1,34 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xP, double yP, double xV,
+                   double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    public double calDistance(Planet p) {
+        double distance;
+        distance = Math.sqrt(Math.pow((this.xxPos - p.xxPos), 2) +
+                                Math.pow((this.yyPos - p.yyPos), 2));
+        return distance;
+    }
+
+}
